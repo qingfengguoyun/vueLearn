4f3c9428dbd2b51317f5dd0520c5c225c53fe3f4
xiang.xi <xxzzing@126.com>
2024-03-12 18:06:28 +0800

    2024-3-12

--- vitelearn311/hello_vue3/src/components/Person.vue
+++ vitelearn311/hello_vue3/src/components/Person.vue
@@ -2,12 +2,10 @@
     <!-- html -->
     <div class="person">
         <!-- 模板中自动调用name.value -->
-        <div>
-            <h2>姓名：{{ name }}</h2>
-            <h2>年龄：{{ age }}</h2>
-        </div>
-        <button @click="change">修改</button>
-
+        <h2> 姓： <input type="text " v-model="person.firstName" ></h2><br>
+        <h2> 名： <input type="text " v-model="person.lastName"></h2><br>
+        <h2> 全名：{{ fullName }}</h2><br>
+        <button @click="changeFullName">changeFullName</button>
         
     </div>
 </template>
@@ -23,20 +21,36 @@
 </script>
 
 <script lang="ts" setup>
-import { ref, toRef ,toRefs} from 'vue';
+    import { ref, toRef ,toRefs,computed} from 'vue';
 
     let person=ref({
-        name:"张三",
-        age:18
+        firstName:"张",
+        lastName:"三"
     })
+    //这种方式的计算属性是不可修改的（只能由计算元素的修改而被动修改）
+    // let fullName=computed(()=>{
+    //     console.log(person.value.firstName)
+    //     let f=person.value.firstName+person.value.lastName
+    //     return f;       
+    // })
 
-    let{name,age}=toRefs(person.value);
 
-    function change(){
-        name.value+="*";
-        age.value+=1;
+    let fullName=computed({
+        get(){
+            return person.value.firstName+person.value.lastName;
+        },
+        set(val){
+            console.log('set',val)
+            let str1=val.slice(0,1)
+            let str2=val.slice(1)
+            person.value.firstName=str1;
+            person.value.lastName=str2;
+        }
+    })
+    function changeFullName(){
+        // console.log(fullName.value)
+        fullName.value="李四";
     }
-
 
 </script>
 
---
+++ vitelearn311/hello_vue3/备份/2024-3-12/15_toRef和toRefs/Person.vue
@@ -0,0 +1,53 @@
+<template>
+    <!-- html -->
+    <div class="person">
+        <!-- 模板中自动调用name.value -->
+        <div>
+            <h2>姓名：{{ person.name }}</h2>
+            <h2>年龄：{{ person.age }}</h2>
+        </div>
+        <button @click="change">修改</button>
+
+        
+    </div>
+</template>
+
+
+
+<script lang="ts" >
+    // js/ts
+    export default
+    {
+        name:'Person'
+    }
+</script>
+
+<script lang="ts" setup>
+import { ref, toRef ,toRefs} from 'vue';
+
+    let person=ref({
+        name:"张三",
+        age:18
+    })
+
+    //解构创建
+    //对解构创建得到的name和age进行修改会同样影响person.name,person.age
+    let{name,age}=toRefs(person.value);
+
+    function change(){
+        name.value+="*";
+        age.value+=1;
+    }
+
+
+</script>
+
+<style>
+    /* 样式 */
+    .person {
+        background-color: #ddd;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px; 
+    }
+</style>
