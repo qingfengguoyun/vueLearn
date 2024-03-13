d06518f5823313f053ba0fca2afd3beb425d7fa7
xiang.xi <xxzzing@126.com>
2024-03-13 19:40:09 +0800

    2024-3-13

---
+++ vitelearn311/hello_vue3/备份/2024-3-13/23_标签的ref属性/utils/WaterCage.ts
@@ -0,0 +1,10 @@
+export default class WaterCage{
+    public temp: number;
+    public height: number;
+    constructor();
+    constructor(temp:number,height:number);
+    constructor(temp?:number,height?:number){
+        this.temp=temp;
+        this.height=height;
+    }
+}
