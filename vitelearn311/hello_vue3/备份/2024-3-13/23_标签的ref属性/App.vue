09929963a702746d67c3004a585d9e053953179f
xiang.xi <xxzzing@126.com>
2024-03-19 20:01:43 +0800

    2024-3-19-2

--- vitelearn311/hello_vue3/备份/2024-3-13/23_标签的ref属性/App.vue
+++ vitelearn311/hello_vue3/备份/2024-3-13/23_标签的ref属性/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="app"> 
     <h1 ref="ref1">hello world</h1>
-    <p>ref属性添加在html标签上时，获取dom元素</p>
+    <!-- <p>ref属性添加在html标签上时，获取dom元素</p> -->
     <button @click="showRef">显示Ref1</button>
     <!-- <Person/> -->
-    <p>ref属性添加在组件标签上时，获取组件，可访问内容通过defineExpose对外暴露</p>
+    <!-- <p>ref属性添加在组件标签上时，获取组件，可访问内容通过defineExpose对外暴露</p> -->
     <Test ref="refTest"/>
     <button @click="showRefTest">显示RefTest</button>
     <!-- <button @click="show">显示tel</button> -->
@@ -21,10 +21,12 @@
   import Person from './components/Person.vue';
   import Test from './components/Test.vue';
   import {ref } from 'vue';
+  //见template中h1标签
   let ref1=ref();
   function showRef(){
     console.log(ref1.value);
   }
+  //见template中的<Test> 组件
   let refTest=ref()
   function showRefTest(){
     console.log(refTest.value);
