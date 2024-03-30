75b8791f378c4a3e6ba5d8fd356e98400b57ed54
bh601 <xxzzing@163.com>
2024-03-30 17:54:14 +0800

    2024-3-30

---
+++ vitelearn311/hello_vue3/src/pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="home">
+      <img :src="getIco()" alt="">
+    </div>
+  </template>
+  
+  <script setup lang="ts" name="Home">
+
+    function getIco(){
+      return new URL('../../public/favicon.ico', import.meta.url).href
+    }
+  </script>
+  
+  <style scoped>
+    .home {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100%;
+    }
+  </style>
