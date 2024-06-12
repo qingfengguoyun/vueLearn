4f78337fa3a44b12b34a9de0c6da86a3e223750a
bh601 <xxzzing@163.com>
2024-06-12 22:29:04 +0800

    重新提交文件名为大写的文件

---
+++ vitelearn311/vue_myProject326/src/components/Gallary.vue
@@ -0,0 +1,17 @@
+
+<template>
+    <h2> This is gallary Component</h2>
+
+</template>
+<script lang='ts'>
+export default
+{
+    name: "Gallary"
+}
+</script>
+<script lang='ts' setup>
+
+</script>
+<style scoped>
+
+</style>
---
+++ vitelearn311/vue_myProject326/src/components/HeaderMenu.vue
@@ -0,0 +1,69 @@
+<template>
+    <nav class="navbar navbar-static-top  " role="navigation" style="margin-bottom: 0">
+        <div class="navbar-header">
+            <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#" @click="sideBar.showSideBar"><i class="fa fa-bars"
+                    ></i> </a>
+            <form role="search" class="navbar-form-custom" action="search_results.html">
+                <div class="form-group">
+                    <input type="text" placeholder="Search for something..." class="form-control" name="top-search"
+                        id="top-search">
+                </div>
+            </form>
+        </div>
+        <ul class="nav navbar-top-links navbar-right">
+            <li>
+                <span class="m-r-sm text-muted welcome-message">欢迎 {{ user.userName }}</span>
+            </li>
+            <!-- <li class="dropdown">
+                <a class="dropdown-toggle count-info" data-toggle="dropdown" href="empty_page.html#">
+                    <i class="fa fa-envelope"></i> <span class="label label-warning">16</span>
+                </a>
+            </li>
+            <li class="dropdown">
+                <a class="dropdown-toggle count-info" data-toggle="dropdown" href="empty_page.html#">
+                    <i class="fa fa-bell"></i> <span class="label label-primary">8</span>
+                </a>
+            </li> -->
+
+
+            <li>
+                <a href="#" @click="logOut">
+                    <i class="fa fa-sign-out"></i> 退出登录
+                </a>
+            </li>
+        </ul>
+
+    </nav>
+</template>
+<script lang='ts'>
+export default
+    {
+        name: "HeaderMenu"
+    }
+</script>
+<script lang='ts' setup>
+import { useRouter, RouterLink } from 'vue-router';
+import { type UserInter } from '@/types/UserType'
+import { useSideBar } from '@/store/sidebar';
+import { useCommonStore } from '@/store/commonStore';
+
+let user = JSON.parse(sessionStorage.getItem("userInfo") as string) as UserInter || {}
+let router = useRouter();
+let sideBar=useSideBar();
+
+
+
+function logOut() {
+    console.log("logout")
+    router.replace({
+        name: "LoginPage"
+    })
+}
+
+</script>
+<style scoped>
+    .header-menu {
+        position: relative;
+        display: inline-flex;
+    }
+</style>
